2cc1828429d9d0c5876dce17cb936963f4cce1ae
Yosuke Iwata <s20970059@nucba.ac.jp>
2023-08-29 22:33:45 +0900

    addを実装しました

--- cpmponents/TodoApp.tsx
+++ cpmponents/TodoApp.tsx
@@ -1,16 +1,49 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import { addTodo, getAllTodos} from "../utiles/supabaseFunctions"
 import TodoList from './TodoList'
 import './components.css'
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState<any>([]);
+  const [title, setTitle] = useState<string>("");
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todos = await getAllTodos();
+      setTodos(todos);
+      console.log(todos);
+    };
+    getTodos();
+  },[]);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if(title === "")return;
+
+    //Todoの追加
+    await addTodo(title);
+    let todos = await getAllTodos();
+    setTodos(todos);
+
+    setTitle("");
+  } ;
+
+
   return (
     <section className="section">
       <h3>Supabase Todo App</h3>
-      <form>
-        <input type="text" className="inputcustom" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input 
+          type="text" 
+          className="inputcustom" 
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          />
         <button className="buttoncustom">Add</button> 
       </form>
-      <TodoList />
+      <TodoList todos={todos}/>
     </section>
   )
 }
--- cpmponents/TodoList.tsx
+++ cpmponents/TodoList.tsx
@@ -1,13 +1,22 @@
+import { Todo } from '@/utiles/interface';
 import React from 'react'
 
-const TodoList = () => {
+type Props = {
+  todos: Todo[];
+}
+
+const TodoList = (props: Props) => {
+  const{ todos } = props;
+
   return (
     <div>
       <ul className="ulcustom">
-        <div className='todolist'>
-          <li>✅読書</li>
+        {todos.map((todo) => (
+        <div key={todo.id}className='todolist'>
+          <li>✅{todo.title}</li>
           <span className='pointer'>✖️</span>
         </div>
+        ))}
       </ul>
     </div>
   )
